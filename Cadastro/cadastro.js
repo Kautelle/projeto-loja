af56f090c3a4443967d83c8deef80b756ea466da
Washington Miscossi <washingtonmiscossi123@gmail.com>
2022-11-11 00:44:00 -0300

    1

---
+++ Cadastro/cadastro.js
@@ -0,0 +1,20 @@
+const card_login = document.querySelector('#card_login')
+const card_cadastro = document.querySelector('#card_cadastro')
+const btn_cor = document.querySelector('.btn_cor')
+
+
+document.querySelector('#card_login')
+    .addEventListener('click',() =>{
+        btn_cor.style.left = "0px"
+        
+}  )
+
+document.querySelector('#card_cadastro')
+    .addEventListener('click', () => {
+        btn_cor.style.left = "111px"
+            
+        
+       
+    })
+
+    
---
+++ Finalizar compra/finalizar.js
@@ -0,0 +1,21 @@
+const card_cartao = document.querySelector('#card_cartao')
+const card_pix = document.querySelector('#card_pix')
+const btn_cor = document.querySelector('.btn_cor')
+
+
+document.querySelector('#card_cartao')
+    .addEventListener('click',() =>{
+        btn_cor.style.left = "0px"
+        
+}  )
+
+document.querySelector('#card_pix')
+    .addEventListener('click', () => {
+        btn_cor.style.left = "76px"
+        
+            
+        
+       
+    })
+
+    
